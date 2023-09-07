ae543232280c594c31a7ddb6ca9568ba967886dd
Wang Jiefan <wng.jiefan@gmail.com>
2023-09-07 16:36:15 +0800

    temp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,39 @@
-import { type ChangeEvent, useState } from "react";
+import { type ChangeEvent, useState, type HTMLInputTypeAttribute } from "react";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
 
 import { api } from "~/utils/api";
 
+interface inpProps {
+  name: string;
+  type?: HTMLInputTypeAttribute;
+  value: string | number;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const StyledInput = ({ name, type, value, onChange }: inpProps) => <td className="border px-4 py-2">
+  <input className="bg-transparent outline-none border-0"
+    name={name}
+    type={type ?? 'text'}
+    value={value}
+    onChange={onChange}
+  />
+</td>;
+
 export default function Home() {
   const questions = api.question.getAll.useQuery();
   const [dummyQuestion, setDummyQuestion] = useState({
-    title: "",
-    body: "",
-    difficulty: "",
-    category: "",
+    title: '',
+    body: '',
+    difficulty: 0,
+    description: '',
   });
   const [newData, setNewData] = useState(false);
 
-  const StyledInputCell = ({ value }: { value: string }) => (
-    <td className="border px-4 py-2">
-      <input className="bg-transparent outline-none border-0" type="text" value={value} />
-    </td>
-  )
+  const createQuestion = () => {
+    // TODO
+  };
 
   return (
     <>
@@ -33,7 +47,7 @@ export default function Home() {
           <h1 className="text-5xl font-extrabold tracking-tight text-[var(--txt-3)] sm:text-[5rem]">
             Peer<span className="text-[var(--txt-1)]">Prep</span>
           </h1>
-          {newData && <button>Add Question</button>}
+          <button onClick={createQuestion} className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20" value="Add Question">Add Question</button>
           <table className="table-auto text-[var(--txt-3)]">
             <thead>
               <tr>
@@ -55,10 +69,14 @@ export default function Home() {
             </tbody>
             <tfoot>
               <tr>
-                <StyledInputCell value={dummyQuestion.title}  />
-                <StyledInputCell value={dummyQuestion.body}  />
-                <StyledInputCell value={dummyQuestion.difficulty}  />
-                <StyledInputCell value={dummyQuestion.category}  />
+                {/* <StyledFormInitInputCell name="title" onSubmit={createQuestion} />
+                <StyledInputCell name="body" />
+                <StyledInputCell name="difficulty" type="number" />
+                <StyledInputCell name="description" /> */}
+                <StyledInput name="title" value={dummyQuestion.title} onChange={(e) => setDummyQuestion({ ...dummyQuestion, title: e.target.value })} />
+                <StyledInput name="body" value={dummyQuestion.body} onChange={(e) => setDummyQuestion({ ...dummyQuestion, body: e.target.value })} />
+                <StyledInput name="difficulty" type="number" value={dummyQuestion.difficulty} onChange={(e) => setDummyQuestion({ ...dummyQuestion, difficulty: Number(e.target.value) })} />
+                <StyledInput name="description" value={dummyQuestion.description} onChange={(e) => setDummyQuestion({ ...dummyQuestion, description: e.target.value })} />
               </tr>
             </tfoot>
           </table>
