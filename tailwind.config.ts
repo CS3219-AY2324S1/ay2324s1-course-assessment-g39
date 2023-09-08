b695912c8e20569385d6f74dd4f36d011e82952f
Au Chen Xi, Gabriel <au.gabriel@u.nus.edu>
2023-09-08 19:10:36 +0800

    Fix colors on signup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,14 @@
 import { type Config } from "tailwindcss";
+import colors from 'tailwindcss/colors';
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      primary: colors.blue,
+      ...colors
+    }
   },
   plugins: [],
 } satisfies Config;
