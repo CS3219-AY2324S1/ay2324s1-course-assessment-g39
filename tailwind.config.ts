44fc99898f5208b6d029e258b88842e63c1974c5
Au Chen Xi, Gabriel <au.gabriel@u.nus.edu>
2023-09-08 19:12:32 +0800

    Switch to extend

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,11 +4,11 @@ import colors from 'tailwindcss/colors';
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-    colors: {
-      primary: colors.blue,
-      ...colors
-    }
+    extend: {
+      colors: {
+        primary: colors.blue
+      }
+    },
   },
   plugins: [],
 } satisfies Config;
