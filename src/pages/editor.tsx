d210c4b1c427b4ea3707acf43bb39e16c0f91ce8
Au Chen Xi, Gabriel Au <e0544006@u.nus.edu>
2023-09-21 14:54:14 +0800

    Basic editor

---
+++ src/pages/editor.tsx
@@ -0,0 +1,32 @@
+import CodeMirror from '@uiw/react-codemirror';
+import { markdown, markdownLanguage } from '@codemirror/lang-markdown';
+import { languages } from '@codemirror/language-data';
+
+const code = `## Title
+
+\`\`\`jsx
+function Demo() {
+  return <div>demo</div>
+}
+\`\`\`
+
+\`\`\`bash
+# Not dependent on uiw.
+npm install @codemirror/lang-markdown --save
+npm install @codemirror/language-data --save
+\`\`\`
+
+[weisit ulr](https://uiwjs.github.io/react-codemirror/)
+
+\`\`\`go
+package main
+import "fmt"
+func main() {
+  fmt.Println("Hello, 世界")
+}
+\`\`\`
+`;
+
+export default function App() {
+  return <CodeMirror value={code} extensions={[markdown({ base: markdownLanguage, codeLanguages: languages })]} />;
+}
