8e5dce539d6ae731b4d96591684203aa041b2477
samyipsh <samyipsh@gmail.com>
2023-09-21 22:45:31 +0800

    profile page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,10 +18,16 @@ export default function Home() {
             Go to Questions
           </Link>
           <Link
-            href="/signup/samyipsh"
+            href="/signup/"
             className="flex items-center justify-center font-bold text-white no-underline transition hover:bg-white/20 rounded-md whitespace-nowrap bg-white/10 flex-[1_0_0%] px-2 py-1 mt-2"
           >
             Go to Signup
+          </Link>
+          <Link
+            href="/profile"
+            className="flex items-center justify-center font-bold text-white no-underline transition hover:bg-white/20 rounded-md whitespace-nowrap bg-white/10 flex-[1_0_0%] px-2 py-1 mt-2"
+          >
+            User Profile
           </Link>
         </div>
       </main>
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -2,6 +2,8 @@ import { NextPage } from "next";
 import Head from "next/head";
 import { LoadingPage } from "~/components/Loading";
 import { api } from "~/utils/api";
+import Image from "next/image";
+import { PageLayout } from "~/components/Layout";
 
 const ProfilePage: NextPage<{ username?: string }> = ({ username }) => {
   username = "sam";
@@ -11,16 +13,27 @@ const ProfilePage: NextPage<{ username?: string }> = ({ username }) => {
   console.log("username: ", username);
   if (isLoading) return <LoadingPage />;
   if (!data) return <div>no such user</div>;
+  const { id, image: imageURL } = data;
+
   return (
     <>
       <Head>
         <title>Profile</title>
       </Head>
-      <main className="flex h-screen justify-center bg-black text-slate-100">
-        <div className="overscroll-y-scroll h-full w-full border-x md:max-w-2xl">
-          <div>Profile page for {username}</div>
+      <PageLayout>
+        <div className="relative h-48 bg-slate-600 border-b overscroll-y-scroll w-full border-x md:max-w-2xl">
+          <Image
+            src={imageURL || "https://picsum.photos/300/300"}
+            alt={`${username ?? ""}'s profile pic`}
+            width={128}
+            height={128}
+            className="absolute -mb-[64px] ml-4 rounded-md border-b border-2 bottom-0 left-0 bg-black"
+          />
         </div>
-      </main>
+        <div className="h-[64px]"></div>
+        <div className="p-4 text-2xl font-bold">@{username}</div>
+        <div className="border-b border-slate-100"></div>
+      </PageLayout>
     </>
   );
 };
