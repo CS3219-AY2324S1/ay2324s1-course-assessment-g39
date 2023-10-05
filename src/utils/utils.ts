4096508eef5c36d277f72f1a5a722fd43baeeb8c
Wang Jiefan <wng.jiefan@gmail.com>
2023-10-05 14:04:20 +0800

    finalize ass 1 + tag

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -4,15 +4,16 @@ import { unified } from "unified";
 import remarkRehype from "remark-rehype";
 import rehypeStringify from "rehype-stringify";
 import rehypeSanitize from "rehype-sanitize";
+import supersub from "remark-supersub";
 
 export const makeMap = <T, K extends keyof T & string>(l: T[], k: K) =>
   new Map(l.map((q) => [q[k], q]));
 
 export const parseMD = async (md: string) =>
-  // Remove when this closes: https://github.com/orgs/remarkjs/discussions/1214
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  String( await unified().use(remarkParse, {fragment: true}).use(remarkGfm).use(remarkRehype)
+  String(// Remove when this closes: https://github.com/orgs/remarkjs/discussions/1214
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    await unified().use(remarkParse, { fragment: true }).use(remarkGfm).use(supersub).use(remarkRehype)
       .use(rehypeSanitize)
       .use(rehypeStringify)
       .process(md),
