5595babda6b06520266d6c533e532187eeb4f71b
samyipsh <samyipsh@gmail.com>
2023-10-05 09:44:56 +0800

    fix

--- start-dev-script.sh
+++ start-dev-script.sh
@@ -3,8 +3,7 @@
 # Check if Docker is running
 if ! docker info &> /dev/null; then
   echo "Docker is not running. Starting Docker..."
-  open -a Docker
-  sleep 5  # Wait for Docker to start (adjust this if needed)
+  open -a Docker && sleep 5 
 fi
 
 # Check if PostgreSQL is running
@@ -13,11 +12,28 @@ if pgrep -x "postgres" &> /dev/null; then
   sudo pkill -u postgres
 fi
 
+# Function to perform cleanup
+cleanup() {
+  echo "Cleaning up..."
+  kill $PRISMA_STUDIO_PID
+  yarn docker:down & 
+  sleep 20
+  echo "Stopping Docker..."
+  pkill "Docker"
+  exit 0
+}
+# Trap Ctrl+C signal and call cleanup function
+trap cleanup EXIT
+
+
 # Run other commands
 yarn
-yarn docker:up
-yarn prisma:studio &
+
+yarn docker:up && sleep 5
+
+yarn prisma:studio -- &
+PRISMA_STUDIO_PID=$!
+
+open http://localhost:3000  
 yarn dev -- -p 3000
 
-# Open the localhost URL in the default web browser
-open http://localhost:3000 
