0dd8d554d32835869593f29b9edfbe7829f3b759
Au Chen Xi, Gabriel <au.gabriel@u.nus.edu>
2023-10-08 01:08:07 +0800

    Switch back to h-screen
    h-full broke user profile

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,7 @@ import { type PropsWithChildren } from "react";
 
 export const PageLayout = (props: PropsWithChildren) => {
   return (
-    <main className="flex h-full justify-center bg-slate-900 text-slate-100 overflow-auto">
+    <main className="flex h-screen justify-center bg-slate-900 text-slate-100 overflow-auto">
       <div className="h-full w-full md:max-w-2xl">
         {props.children}
       </div>
