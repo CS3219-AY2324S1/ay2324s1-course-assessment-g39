b6005626c919e3d13972cbcf52c25b41fd0c64ba
samyipsh <samyipsh@gmail.com>
2023-10-07 11:23:57 +0800

    add rect logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
-import { PeerPrepLogo } from "~/assets/logo";
+import { PeerPrepCircularLogo, PeerPrepRectLogo } from "~/assets/logo";
 
 export default function Home() {
   return (
@@ -13,7 +13,8 @@ export default function Home() {
       </Head>
       <main className=" flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[var(--bg-1)] to-[var(--bg-2)]">
         <div className="container flex flex-col items-center justify-center px-12 py-28 max-w-screen-xl">
-          <PeerPrepLogo height={300} width={300} fill="black" />
+          <PeerPrepCircularLogo height={300} width={300} fill="black" />
+          <PeerPrepRectLogo height={300} width={300} fill="" />
           <Link
             href="/questions"
             className="flex items-center justify-center font-bold text-white no-underline transition hover:bg-white/20 rounded-md whitespace-nowrap bg-white/10 flex-[1_0_0%] px-2 py-1 mt-2"
