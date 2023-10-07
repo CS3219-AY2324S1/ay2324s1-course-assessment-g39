31aa23f7aa0d65ded391e75f43cfef6ffa8316ef
Au Chen Xi, Gabriel <au.gabriel@u.nus.edu>
2023-10-08 00:55:00 +0800

    Fix page layout overflow

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,8 +2,8 @@ import { type PropsWithChildren } from "react";
 
 export const PageLayout = (props: PropsWithChildren) => {
   return (
-    <main className="flex h-100 justify-center bg-black text-slate-100">
-      <div className="overscroll-y-scroll h-full w-full border-x md:max-w-2xl">
+    <main className="flex h-100 justify-center bg-black text-slate-100 overflow-auto">
+      <div className="h-full w-full border-x md:max-w-2xl">
         {props.children}
       </div>
     </main>
