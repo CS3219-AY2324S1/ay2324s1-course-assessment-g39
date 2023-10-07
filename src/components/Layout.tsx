90aecaefda07a45a58b37df32e71799b51fae434
Au Chen Xi, Gabriel <au.gabriel@u.nus.edu>
2023-10-08 00:53:31 +0800

    Fix react svg tags

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,7 @@ import { type PropsWithChildren } from "react";
 
 export const PageLayout = (props: PropsWithChildren) => {
   return (
-    <main className="flex h-screen justify-center bg-black text-slate-100">
+    <main className="flex h-100 justify-center bg-black text-slate-100">
       <div className="overscroll-y-scroll h-full w-full border-x md:max-w-2xl">
         {props.children}
       </div>
