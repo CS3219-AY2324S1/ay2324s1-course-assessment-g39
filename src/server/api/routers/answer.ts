8d47e3cd1790646e8db5ff44b2e91027450c06e6
Au Chen Xi, Gabriel <au.gabriel@u.nus.edu>
2023-10-15 12:34:03 +0800

    Integrate with judge

--- src/server/api/routers/answer.ts
+++ src/server/api/routers/answer.ts
@@ -1,19 +1,48 @@
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
-import { prismaMongo, prismaPostgres } from "~/server/db";
+import { PrismaMongoT, prismaMongo, prismaPostgres, PrismaPostgresT } from "~/server/db";
 import type { AnswerResult } from "@prisma-db-psql/client";
-import type { Answer } from "@prisma-db-mongo/client";
+import type { Answer, TestCase } from "@prisma-db-mongo/client";
 import { TRPCError } from "@trpc/server";
+import { judgeRouter } from "./judge";
+import { appRouter } from "../root";
+import { Session } from "next-auth";
 
 const createAnswerInput = z.object({
     body: z.string(),
-    questionId: z.string(),
-    userId: z.string()
+    userId: z.string(),
+    environmentId: z.string()
 });
+async function runAnswer(answer: Answer, ctx: {
+    session: Session | null,
+    prismaPostgres: PrismaPostgresT,
+    prismaMongo: PrismaMongoT
+}): Promise<{
+    ans: AnswerResult
+}> {
+    const testCases = await prismaMongo.testCase.findMany({
+        where: {
+            environmentId: answer.envId
+        }
+    })
+    let allCorrect = true;
+    
+    const caller = appRouter.createCaller(ctx);
+    // todo: create  sumission batch
+    for (const testCase of testCases) {
+        const result = await caller.judge.runTestCase({
+            testCaseId: testCase.id,
+            source_code: answer.body
+        });
+        if (result.status.id !== 3) {
+            allCorrect = false;
+        }
+    }
+    // todo: support other fields
+    return {
+        ans: allCorrect ? "ACCEPTED" : "WRONG_ANSWER"
+    }
 
-function runAnswer(answer: Answer): AnswerResult {
-    // todo: implement this
-    return "ACCEPTED";
 }
 
 const answerRouter = createTRPCRouter({
@@ -23,22 +52,31 @@ const answerRouter = createTRPCRouter({
     submitAnswer: protectedProcedure
         .input(createAnswerInput)
         .mutation(async ({ ctx, input }) => {
-            const { body, questionId } = input;
+            const { body, environmentId } = input;
             const user = ctx.session.user;
+            const env = await prismaMongo.environment.findUnique({
+                where: {
+                    id: input.environmentId
+                }
+            });
+            if (!env) throw new TRPCError({
+                code: "BAD_REQUEST",
+                message: "Invalid environment"
+            })
             const answer = await prismaMongo.answer.create({
                 data: {
                     body,
-                    questionId
+                    envId: environmentId
                 }
             });
             // todo: generate the result from using some kind of code execution
-            const result: AnswerResult = runAnswer(answer);
+            const result = await runAnswer(answer, ctx);
             const userAnswer = await prismaPostgres.questionAttempt.create({
                 data: {
                     answerId: answer.id,
-                    questionId,
+                    questionId: env?.questionId,
                     userId: user.id,
-                    result
+                    result: result.ans
                 }
             })
             return userAnswer;
