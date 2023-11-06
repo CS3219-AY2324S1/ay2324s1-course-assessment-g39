36dcad2f8855f5acc706de29d15c72c3cff9c67b
samyipsh <samyipsh@gmail.com>
2023-11-06 22:00:00 +0800

    [fix] modal was blocking menu

--- src/pages/collab/index.tsx
+++ src/pages/collab/index.tsx
@@ -79,7 +79,7 @@ const MatchRequestPage = () => {
     const handleBeforeUnload = (e: BeforeUnloadEvent) => {
       if (curUserMatchRequest) {
         e.preventDefault();
-        // void handleDeleteMatchRequest();
+        void handleDeleteMatchRequest();
       }
     };
 
@@ -619,28 +619,30 @@ const RequestStatus = (props: { matchType: MatchType }) => {
       ? "Finding a request for you"
       : "Pending request acceptance";
   return (
-    <div className="fixed top-0 w-full z-50 animate-pulse flex items-center justify-center p-5">
-      <div className="bg-slate-200 rounded-md text-slate-800 px-6 py-3 flex items-center">
-        <svg
-          className="animate-spin h-5 w-5 text-slate-600 mr-3"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <circle
-            className="opacity-25"
-            cx="12"
-            cy="12"
-            r="10"
-            stroke="currentColor"
-            strokeWidth="4"
-          ></circle>
-          <path
-            className="opacity-75"
-            fill="currentColor"
-            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
-          ></path>
-        </svg>
-        {msg}
+    <div className="fixed top-0 left-0 translate-x-1/2 translate-y-1/2 w-1/2">
+      <div className="animate-pulse flex items-center justify-center">
+        <div className="bg-slate-200 rounded-md text-slate-800 px-6 py-3 flex items-center">
+          <svg
+            className="animate-spin h-5 w-5 text-slate-600 mr-3"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              strokeWidth="4"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
+            ></path>
+          </svg>
+          {msg}
+        </div>
       </div>
     </div>
   );
