e375c4babfa122f7db017b8f7d333d3df58d3748
Au Chen Xi, Gabriel <au.gabriel@u.nus.edu>
2023-11-09 13:33:11 +0800

    Add serverless leetcode function

---
+++ serverless/index.js
@@ -0,0 +1,130 @@
+const functions = require('@google-cloud/functions-framework');
+const { Schema, default: mongoose } = require('mongoose');
+// For Node.js
+var TurndownService = require('turndown')
+
+const turndownService = new TurndownService();
+const leetcodeURL = "https://leetcode.com/graphql";
+
+
+
+const questionListQuery = (totalnum) => `
+{
+  questionList(categorySlug: "", skip: 0, filters: {}, limit: ${totalnum}) {
+  total: totalNum
+  questions: data {
+    acRate
+    difficulty
+    freqBar
+    frontendQuestionId: questionFrontendId
+    isFavor
+    paidOnly: isPaidOnly
+    status
+    title
+    content
+    titleSlug 
+    topicTags {
+      name
+      id
+      slug
+    }
+    hasSolution
+    hasVideoSolution
+  }
+}
+}
+`
+const questionsSchema = new Schema({
+  _id: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date
+  },
+  title: String,
+  body: String,
+  difficulty: ["EASY", "MEDIUM", "HARD"],
+  category: {
+    type: String,
+    default: ""
+  }
+});
+
+mongoose.set('setDefaultsOnInsert', true);
+functions.http('leetcode', async (req, res) => {
+
+  const totalNum = await fetch(leetcodeURL, {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      query: questionListQuery(1)
+    })
+  })
+  const mgdb = await mongoose.connect(process.env.MONGO_URL);
+  const Question = mgdb.model("Question", questionsSchema, "Question");
+  
+  const tm = await totalNum.json();
+  const actualNum = tm.data.questionList.total;
+  const questionList = await fetch(leetcodeURL, {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      query: questionListQuery(actualNum)
+    })
+  });
+  // update the data
+  const result = await questionList.json();
+  const questionListObject = result.data.questionList.questions;
+  const resultMap = questionListObject.filter(d => d.content !== null).map((data) => {
+    return {
+      difficulty: data.difficulty,
+      title: data.title,
+      titleSlug: data.titleSlug,
+      content: turndownService.turndown(data.content)
+    }
+  });
+  await Question.collection.bulkWrite(
+    resultMap.map((data) => {
+      return {
+        'updateOne': {
+          'filter': {
+            'id': data.titleSlug
+          },
+          'update': {
+            '$set': {
+              title: data.title,
+              body: data.content,
+              difficulty: data.difficulty.toUpperCase(),
+              updatedAt: new Date(),
+            },
+            '$setOnInsert': {   
+              _id: data.titleSlug,
+              createdAt: new Date(),
+              category: "",
+            }
+          },
+          'upsert': true
+        },
+      }
+    })
+  ).catch((e) => {
+    res.json({
+      message: "Failed to update db",
+    })
+    res.end();
+  });
+  // todo: perform the repository update here
+  const responseObj = {
+    data: resultMap
+  };
+  res.json({
+    message: "Success"
+  });
+  
+})
